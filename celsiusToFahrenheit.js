eace542456e89c2868defa70ea705177ba4d7834
mdsadikul <mdsadikulislam891@gamil.com>
2022-08-06 19:12:43 +0600

    added celsius to fahrenheit

---
+++ celsiusToFahrenheit.js
@@ -0,0 +1,12 @@
+
+// Celsius to fahrenheit convertion
+function celsiusToFahrenheit(celsius){
+    let fahrenheit = celsius * 9 / 5 + 32;
+    return fahrenheit;
+}
+
+const fahrenheits = celsiusToFahrenheit(30);
+console.log('Celsius To Fahrenheit:', fahrenheits);
+
+
+// Fahren
